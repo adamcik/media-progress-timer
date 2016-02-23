7ae72cbc554dde7ca4a6596460ddf4477f26ee4a
Thomas Adamcik <thomas@adamcik.no>
2016-02-23 01:21:44 +0100

    Try ang explain more of the code with some comments

--- timer.js
+++ timer.js
@@ -7,15 +7,20 @@
 
 'use strict';
 
+// Helper function to provide the current time in milliseconds.
 var now = typeof window.performance !== 'undefined' &&
           typeof window.performance.now !== 'undefined' &&
           window.performance.now.bind(window.performance) || Date.now ||
           function() { return new Date().getTime(); };
 
+// Helper to warn library users about deprecated features etc.
 var warn = function(msg) {
     setTimeout(function() { throw msg; }, 0);
 };
 
+// Creates a new timer object, works with both 'new ProgressTimer(options)' and
+// just 'ProgressTimer(options). Optionally the timer can also be called with
+// only the callback instead of options.
 function ProgressTimer(options) {
     if (!(this instanceof ProgressTimer)) {
         return new ProgressTimer(options);
@@ -27,6 +32,8 @@ function ProgressTimer(options) {
         throw 'ProgressTimer needs a callback to operate.';
     }
 
+    this._userCallback = options.callback;
+
     if (options.updateRate && !options.fallbackTargetFrameRate) {
         warn('ProgressTimer no longer supports the updateRate option.');
         this._fallbackRate = Math.max(options.updateRate, 10);
@@ -35,31 +42,35 @@ function ProgressTimer(options) {
     }
 
     this._running = false;
-    this._userCallback = options.callback;
 
     var useFallback = typeof window.requestAnimationFrame === 'undefined' ||
                       options.disableRequestAnimationFrame || false;
-
     if (!useFallback) {
         this._callUpdate = this._scheduleAnimationFrame;
         this._scheduleUpdate = this._scheduleAnimationFrame;
     }
 
+    // Pre-bind these functions as the get called a lot.
     this._boundCallUpdate = this._callUpdate.bind(this);
     this._boundUpdate = this._update.bind(this);
 
+    // TODO: document what this initializes
     this.reset();
 }
 
+// Updates the timer state to set the position and optionally the duration.
 ProgressTimer.prototype.set = function(position, duration) {
     if (arguments.length === 0) {
         throw 'set requires at least a position argument.';
     } else if (arguments.length === 1) {
+        // Fallback to previous duration, whatever that was.
         duration = this._state.duration;
     } else {
+        // Round down and make sure zero and null are treated as inf.
         duration = Math.floor(
-            Math.max(duration === null ? Infinity : duration || 0, 0));
+            Math.max(duration === null ? Infinity : duration || Infinity, 0));
     }
+    // Make sure '0 <= position <= duration' always holds.
     position = Math.floor(Math.min(Math.max(position || 0, 0), duration));
 
     this._state = {
@@ -73,42 +84,55 @@ ProgressTimer.prototype.set = function(position, duration) {
     return this;
 };
 
+// Marks the timer as running and then requests updates be scheduled.
 ProgressTimer.prototype.start = function() {
     this._running = true;
     this._callUpdate();
     return this;
 };
 
+// Marks the timer as stopped, and then does and explicit 'set()'. This should
+// both ensure that initialPosition gets set to the current position and make
+// sure the _userCallback gets triggered with this position.
 ProgressTimer.prototype.stop = function() {
     this._running = false;
     var state = this._state;
+    // TODO: check if the set() call is really needed any more, might have been
+    // just related to the updateRate option...
     return this.set(state.previousPosition, state.duration);
 };
 
+// Marks the timer as stopped and sets position to zero and duration to inf.
 ProgressTimer.prototype.reset = function() {
     this._running = false;
     return this.set(0, Infinity);
 };
 
+// Internal fallback code for calling the _update method.
 ProgressTimer.prototype._callUpdate = function() {
     this._update(now());
 };
 
+// Internal fallback for scheduling the next update, expects to get called with
+// the timestamp of when we started handling the last frame.
 ProgressTimer.prototype._scheduleUpdate = function(timestamp) {
     var adjustedTimeout = Math.round(timestamp + this._fallbackRate - now());
     setTimeout(this._boundCallUpdate, adjustedTimeout);
 };
 
+// Internal modern code path for triggering updates via RAF.
 ProgressTimer.prototype._scheduleAnimationFrame = function() {
     window.requestAnimationFrame(this._boundUpdate);
 };
 
+// Calls the user callback with the current position/duration and then
+// schedules the next update run via _scheduleUpdate if we haven't finished.
 ProgressTimer.prototype._update = function(timestamp) {
     if (!this._running) {
         return;
     }
 
-    var state = this._state;
+    var state = this._state;  // Copy the state in case it gets replaced.
     state.initialTimestamp = state.initialTimestamp || timestamp;
 
     var position = state.initialPosition + timestamp - state.initialTimestamp;
